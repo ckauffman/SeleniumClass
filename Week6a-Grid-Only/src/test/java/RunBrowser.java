b40dae0c7f128630915b65085de8d02a3cf297bd
Colt Dorsey <cdorsey@xpanxion.com>
2015-07-28 10:35:22 -0500

    Week6a-Grid-Only completion

--- Week6a-Grid-Only/src/test/java/RunBrowser.java
+++ Week6a-Grid-Only/src/test/java/RunBrowser.java
@@ -44,7 +44,14 @@ public class RunBrowser {
             }
         }
 
-        //Add in code to run on the grid here
+        if(runToGrid) {
+            try {
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
+
+            } catch (MalformedURLException e) {
+                fail("Error building URL to Hub");
+            }
+        }
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
