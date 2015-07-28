0da35a2a99ab89ed92545cb29a3f45946a33adc1
Colt Dorsey <cdorsey@xpanxion.com>
2015-07-28 10:34:27 -0500

    week6b-grid-and-jenkins completion

--- Week6b-Grid-And-Jenkins/src/test/java/RunBrowser.java
+++ Week6b-Grid-And-Jenkins/src/test/java/RunBrowser.java
@@ -14,7 +14,6 @@ import static com.thoughtworks.selenium.SeleneseTestBase.fail;
 
 public class RunBrowser {
     WebDriver driver;
-    //String workingDir = "C:\\Users\\cdorsey\\Documents\\workspace\\seleniumclass\\Week6b-Grid-And-Jenkins\\drivers\\";
     String workingDir = System.getProperty("user.dir");
 
     public RunBrowser(String driverType, boolean runToGrid) {
@@ -22,7 +21,7 @@ public class RunBrowser {
 
         if (driverType.equalsIgnoreCase("IE")) {
             System.setProperty("webdriver.ie.driver", workingDir + "\\drivers\\IEDriverServer.exe");
-            //System.setProperty("webdriver.ie.driver", workingDir + "IEDriverServer.exe");
+
             //Disable Internet Explorer security errors for IE 9-10
             desiredCapabilities = DesiredCapabilities.internetExplorer();
             desiredCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
@@ -35,6 +34,7 @@ public class RunBrowser {
                 driver = new ChromeDriver();
             } else {
                 desiredCapabilities = DesiredCapabilities.chrome();
+                desiredCapabilities.setBrowserName("chrome");
             }
         } else {
             if (!runToGrid) {
@@ -43,10 +43,11 @@ public class RunBrowser {
                 desiredCapabilities = DesiredCapabilities.firefox();
             }
         }
+
         if(runToGrid) {
             try {
                 driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
-
+                driver.manage().window().maximize();
             } catch (MalformedURLException e) {
                 fail("Error building URL to Hub");
             }
