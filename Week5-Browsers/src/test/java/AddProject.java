ec7543f9abf21af896ab11cd0b951a8b1268c6c9
timcriffield <tcriffield@xpanxion.com>
2015-08-30 19:18:19 -0500

    Updated the files for assignment 5

--- Week5-Browsers/src/test/java/AddProject.java
+++ Week5-Browsers/src/test/java/AddProject.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitWebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class AddProject {
